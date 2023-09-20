3f3521d75f050a21fc3a78669bb661988cd7695a
EXPANDEDdong <william.sjodin05@gmail.com>
2023-09-20 21:43:20 +0200

    test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,6 @@ export default {
   out: "./drizzle",
   driver: 'mysql2',
   dbCredentials: {
-    connectionString: 'mysql://ahhwahznmef9iqjrvkmn:pscale_pw_rawfkp3aQjPG5HNZ1zO3xcwa2ZhrwEhiI6i6VJAaVKf@aws.connect.psdb.cloud/kvitter?ssl={"rejectUnauthorized":true}',
+    connectionString: '',
   }
 } satisfies Config;
